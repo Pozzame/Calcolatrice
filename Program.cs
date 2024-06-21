42f003868139b95a89fb14cb23ff62a02a2c1d93
Pozzame <pozzame@gmail.com>
2024-06-21 15:45:54 +0200

    Fatto

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Inserisci il primo numero.");
+double num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Inserisci il secondo numero.");
+double num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Inserisci l'operatore tra +-*/");
+string? operatore = Console.ReadLine();
+
+switch (operatore)
+{
+    case "+":
+        Console.WriteLine($"{num1} {operatore} {num2} = {num1 + num2}");
+        break;
+    case "-":
+        Console.WriteLine($"{num1} {operatore} {num2} = {num1 - num2}");
+        break;
+    case "*":
+        Console.WriteLine($"{num1} {operatore} {num2} = {num1 * num2}");
+        break;
+    case "/":
+        if (num2 == 0) Console.WriteLine("Non si può dividere per zero.");
+        else Console.WriteLine($"{num1} {operatore} {num2} = {num1 / num2}");
+        break;
+    default:
+        Console.WriteLine($"Spiacente ma {operatore} non è un operatore valido");
+        break;
+}
