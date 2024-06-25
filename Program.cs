340954ee14fdeceb699bc8f9a670cb9282eb823b
Pozzame <pozzame@gmail.com>
2024-06-25 12:36:21 +0200

    Convert ToDouble

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Inserisci il primo numero.");
-double num1 = Convert.ToInt32(Console.ReadLine());
+double num1 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Inserisci il secondo numero.");
-double num2 = Convert.ToInt32(Console.ReadLine());
+double num2 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Inserisci l'operatore tra +-*/");
 string? operatore = Console.ReadLine();
 
