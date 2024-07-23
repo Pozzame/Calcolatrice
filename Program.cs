b2d69915988e7a8036e6b51a4969de4e68fa97ac
Pozzame <pozzame@gmail.com>
2024-07-23 14:13:52 +0200

    Ok

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
-﻿Console.WriteLine("Inserisci il primo numero.");
+﻿using System;
+
+Console.WriteLine("Inserisci il primo numero.");
 double num1 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Inserisci il secondo numero.");
 double num2 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Inserisci l'operatore tra +-*/");
+Console.WriteLine("Inserisci l'operatore tra +-*/^V");
 string? operatore = Console.ReadLine();
 
 switch (operatore)
@@ -19,6 +21,12 @@ switch (operatore)
     case "/": //Divisione
         if (num2 == 0) Console.WriteLine("Non si può dividere per zero."); //Divisione per zero
         else Console.WriteLine($"{num1} {operatore} {num2} = {num1 / num2}");
+        break;
+    case "^": //Potenza
+        Console.WriteLine($"{num1} {operatore} {num2} = {Math.Pow(num1, num2)}");
+        break;
+    case "V": //Radice n-esima
+    Console.WriteLine($"{num1} {operatore} {num2} = {Math.Pow(num2, 1/num1)}");
         break;
     default: //Operatore errato
         Console.WriteLine($"Spiacente ma '{operatore}' non è un operatore valido");
